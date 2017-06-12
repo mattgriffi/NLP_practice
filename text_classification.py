c880177a9cb6ce915ced7f95119efd9815891991
mseffner <mseffner@gmail.com>
2017-06-11 22:47:29 -0700

    testing accuracy of various ML algorithms with default parameters

---
+++ text_classification.py
@@ -0,0 +1,67 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews, stopwords
+from nltk.classify.scikitlearn import SklearnClassifier
+
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+
+
+def main():
+
+    with open('english_dictionary.txt') as dict_file:
+        english_words = {word.strip().lower() for word in dict_file}
+
+    documents = [(list(movie_reviews.words(fileid)), category)
+                 for category in movie_reviews.categories()
+                 for fileid in movie_reviews.fileids(category)]
+
+    random.shuffle(documents)
+
+    eng_stop_words = set(stopwords.words('english'))
+
+    all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+
+    word_features = [w[0] for w in all_words.most_common(3000)
+                     if w[0] not in eng_stop_words and len(w[0]) > 3 and w[0] in english_words]
+
+    featuresets = [(find_features(rev, word_features), category)
+                   for (rev, category) in documents]
+
+    training_set = featuresets[:1900]
+    testing_set = featuresets[1900:]
+
+    # nb_classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+    algorithm_list = [MultinomialNB, BernoulliNB, LogisticRegression, SGDClassifier, SVC,
+                      LinearSVC, NuSVC]
+    trained_algorithm_list = create_and_train_algorithms(algorithm_list, training_set)
+    test_algorithm_accuracy(trained_algorithm_list, testing_set)
+
+
+def create_and_train_algorithms(algorithm_list, training_set):
+    """Takes a list of machine learning algorithm constructor functions. Instantiates each of
+    the algorithms, trains them with the given training set, and returns the list of trained
+    algorithms."""
+    training_list = [SklearnClassifier(algorithm()) for algorithm in algorithm_list]
+    for algorithm in training_list:
+        algorithm.train(training_set)
+    return training_list
+
+
+def test_algorithm_accuracy(algorithm_list, testing_set):
+    """Tests and prints the accuracy of each algorithm in the list."""
+    for algorithm in algorithm_list:
+        print(f"{algorithm._clf.__class__.__name__:<20} {nltk.classify.accuracy(algorithm, testing_set)}")
+
+
+def find_features(document, word_features):
+    """Returns a dictionary of word-bool pairs. The words are taken from word_features, and
+    the bool tells whether or not that word exists in the given document."""
+    doc_words = set(document)
+    return {word: (word in doc_words) for word in word_features}
+
+
+if __name__ == "__main__":
+    main()
